63a93a09eb936e53ca83d414d9e6046996746921
Anshul Chauhan <anshulchauhan@outlook.com>
2026-03-03 16:18:35 +0530

    Add highlight js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,23 @@
 import { IdAttributePlugin, InputPathToUrlTransformPlugin, HtmlBasePlugin } from "@11ty/eleventy";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
-import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import markdownIt from "markdown-it";
+import markdownItHighlightJS from "markdown-it-highlightjs";
 
 import pluginFilters from "./_config/filters.js";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function(eleventyConfig) {
+	// Markdown-it with HighlightJS
+	const mdOptions = {
+		html: true,
+		breaks: true,
+		linkify: true,
+	};
+	const mdLib = markdownIt(mdOptions).use(markdownItHighlightJS);
+	eleventyConfig.setLibrary("md", mdLib);
+
 	// Drafts, see also _data/eleventyDataSchema.js
 	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
 		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
@@ -50,9 +60,6 @@ export default async function(eleventyConfig) {
 	});
 
 	// Official plugins
-	eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-		preAttributes: { tabindex: 0 }
-	});
 	eleventyConfig.addPlugin(pluginNavigation);
 	eleventyConfig.addPlugin(HtmlBasePlugin);
 	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
